a9ccd2c6ea90d8154f1148bb44a29afb0e98256f
SachinthanaK <kumaranayakages@gmail.com>
2024-09-30 20:00:30 +0530

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import "./App.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
-function App() {
-  return <div></div>;
-}
+import Home from "./home/Home.jsx";
+import Login from "./login/Login.jsx";
+import Dashboard from "./Dashboard/Dashboard.jsx";
 
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/home" exact>
+          <Home />
+        </Route>
+        <Route path="/login" exact>
+          <Login />
+        </Route>
+        <Route path="/dashboard" exact>
+          <Dashboard />
+        </Route>
+        <Route path="/orderhistory" exact></Route>
+        <Route path="/places/new" exact></Route>
+        <Redirect to="/home" />
+      </Switch>
+    </Router>
+  );
+};
 export default App;
