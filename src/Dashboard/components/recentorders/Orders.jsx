5a43393ad94423e198a0c770b3fb76fbc74629c4
SachinthanaK <kumaranayakages@gmail.com>
2024-10-01 22:55:39 +0530

    login v2.0

--- src/Dashboard/components/recentorders/Orders.jsx
+++ src/Dashboard/components/recentorders/Orders.jsx
@@ -5,12 +5,12 @@ import OrdersList from "./OrdersList";
 const Orders = () => {
   const ORDERS = [
     {
-      id: "u1",
-      state: "Urgent",
-      date: "2024/10/12",
-      qty: "500",
-      size: "3x3",
-      est_Date: "2024/12/30",
+      // id: "u1",
+      // state: "Urgent",
+      // date: "2024/10/12",
+      // qty: "500",
+      // size: "3x3",
+      // est_Date: "2024/12/30",
     },
   ];
 
--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { FaRegUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+
+import "./Login.css";
 
 const Login = () => {
-  return <h2>Login Page Works!</h2>;
+  return (
+    <div className="wrapper">
+      <form action=" ">
+        <h1>Login</h1>
+
+        <div className="input-box">
+          <input type="text" placeholder="Username" required />
+          <FaRegUser className="icon" />
+        </div>
+        <div className="input-box">
+          <input type="password" placeholder="Password" required />
+          <FaLock className="icon" />
+        </div>
+
+        <div className="remember-forgot">
+          <label>
+            <input type="checkbox" /> Remember me{" "}
+          </label>
+          <a href="#"> Forget password? </a>
+        </div>
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
