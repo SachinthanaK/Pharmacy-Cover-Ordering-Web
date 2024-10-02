e7f9ca4e7b451a16e9f2dde5a8154a063628dc79
SachinthanaK <kumaranayakages@gmail.com>
2024-10-02 20:37:52 +0530

    working on navbar

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Sidebar from "../navbar/Sidebar";
+
+import "../Dashboard/Dashboard.css";
 
 const Dashboard = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
 };
 
 export default Dashboard;
--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -6,28 +6,30 @@ import "./Login.css";
 
 const Login = () => {
   return (
-    <div className="wrapper">
-      <form action=" ">
-        <h1>Login</h1>
+    <div className="background">
+      <div className="wrapper">
+        <form action=" ">
+          <h1>Login</h1>
 
-        <div className="input-box">
-          <input type="text" placeholder="Username" required />
-          <FaRegUser className="icon" />
-        </div>
-        <div className="input-box">
-          <input type="password" placeholder="Password" required />
-          <FaLock className="icon" />
-        </div>
+          <div className="input-box">
+            <input type="text" placeholder="Username" required />
+            <FaRegUser className="icon" />
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder="Password" required />
+            <FaLock className="icon" />
+          </div>
 
-        <div className="remember-forgot">
-          <label>
-            <input type="checkbox" /> Remember me{" "}
-          </label>
-          <a href="#"> Forget password? </a>
-        </div>
+          <div className="remember-forgot">
+            <label>
+              <input type="checkbox" /> Remember me{" "}
+            </label>
+            <a href="#"> Forget password? </a>
+          </div>
 
-        <button type="submit">Login</button>
-      </form>
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </div>
   );
 };
